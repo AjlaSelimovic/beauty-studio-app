8777f1b1adc785c00ae39274d5b52cbbae7a2930
AjlaSelimovic <ajla.selimovic@stu.ibu.edu.ba>
2023-05-29 18:23:44 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeautyComponent } from './beauty/beauty.component';
+import { ComponentNameComponent } from './component-name/component-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeautyComponent
+    BeautyComponent,
+    ComponentNameComponent
   ],
   imports: [
     BrowserModule,
